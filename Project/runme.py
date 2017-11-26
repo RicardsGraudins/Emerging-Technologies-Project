020a5c8327c27b05dd488170f9fbc9f71763ab08
Ricards <G00314715@gmit.ie>
2017-11-26 17:18:55 +0000

    Upload file

--- Project/runme.py
+++ Project/runme.py
@@ -1,9 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+#folder location for uploaded images
+UPLOAD_FOLDER = 'C:/Users/Richard/Desktop/Images'
+#type of files that can be uploaded
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+#References
+#http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
+
+#setting allowed files
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+	if request.method == 'POST':
+		# check if the post request has the file part
+		if 'file' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
+		file = request.files['file']
+		# if user does not select file, can
+		# submit an empty post without filename
+		if file.filename == '':
+			flash('No selected file')
+			return redirect(request.url)
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			return render_template('Uploaded.html')
 	return render_template('Home.html')
 
 if __name__ == "__main__":
