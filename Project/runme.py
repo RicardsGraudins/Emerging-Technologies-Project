101b28a689ca1d818bd7a297adf2c4c9f0098c67
Ricards <G00314715@gmit.ie>
2017-11-26 19:22:58 +0000

    FAQ & CSS

--- Project/runme.py
+++ Project/runme.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 #folder location for uploaded images
 UPLOAD_FOLDER = 'C:/Users/Richard/Desktop/Images'
 #type of files that can be uploaded
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = 'mysecret'
@@ -25,12 +25,12 @@ def index():
 	if request.method == 'POST':
 		#flash message & redirect if user tries to upload a file outside of the ALLOWED_EXTENSIONS
 		if 'file' not in request.files:
-			flash('This type of file cannot be uploaded.')
+			flash('This type of file cannot be uploaded.\n Please upload a file that uses one of the following formats .png .jpg .jpeg')
 			return redirect(request.url)
 		file = request.files['file']
 		#flash message & redirect if user clicks button without selecting a file to upload
 		if file.filename == '':
-			flash('No selected file')
+			flash('No Selected File')
 			return redirect(request.url)
 		#if the file meets the criteria - upload to the UPLOAD_FOLDER & render template Uploaded.html
 		if file and allowed_file(file.filename):
@@ -38,6 +38,10 @@ def index():
 			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 			return render_template('Uploaded.html')
 	return render_template('Home.html')
+	
+@app.route('/FAQ')
+def faq():
+	return render_template('FAQ.html')
 
 if __name__ == "__main__":
 	app.run(debug=True)
